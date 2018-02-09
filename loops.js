3f18346524cee9bfacd1348042d2188e2c803c34
Oghene Michael <zonalmeet@gmail.com>
2018-02-09 07:26:26 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 function forLoop(array) {
-  return for (let i=0; i < 25; i++) {
-   array.push("`I am ${i} strange loop${i === 0 ? '' : 's'}.`")
+  for (let i=0; i < 25; i++) {
+    array.push("`I am ${i} strange loop${i === 0 ? '' : 's'}.`")
   }
 }
